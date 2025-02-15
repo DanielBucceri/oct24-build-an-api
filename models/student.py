448d953a7e060fb20d690359b07e1af4ad752a06
Daniel <gcas012125@coderacademy.edu.au>
2025-02-15 13:41:52 +1100

    starting student table

---
+++ models/student.py
@@ -0,0 +1,16 @@
+from init import db,ma
+class Student(db.model):    #inherit db.Model "db" defined in init.py as SQLAlchemy()
+    __tablename__ = "students"
+    
+    id = db.Column(db.Integer, primary_key=True) 
+    
+    name = db.Column(db.String(100), nullable=False)   
+    email = db.Column(db.String(200), nullable=False, unique=True)  
+    address = db.Column(db.String(500))
+    
+    
+class StudentSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "name", "email","address")
+        
+StudentSchema(many=True).dump()   
